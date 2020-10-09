f10ef66348d741ba52a10783376bfa76fa420f46
Selena Siri <selenasiri@gmail.com>
2020-10-09 18:36:39 -0400

    changes

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+require('./db');
+const cors = require('cors');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+// Init Middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+//Define Routes
+
+app.use('/apiauth', require('./routes/api/auth.js'));
+
+// app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
 
