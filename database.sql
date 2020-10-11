9a25c99f88c34dd79d4725df2d28a23953ff1f0f
Selena Siri <selenasiri@gmail.com>
2020-10-10 22:42:10 -0400

    add sql env

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE pern_devconn_database;
+\c pern_devconn_database
+--users
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  avatar VARCHAR(255),
+  date TIMESTAMPTZ DEFAULT Now()
+);
+
+--fake users data
+
+insert into users (name, email, password) values ('test11', 'test11@gmail.com', 'test123');
